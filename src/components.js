7e3ae0b70716d3ff44b54783da87dfc0fc553975
frank pagan <frank@cocreate.app>
2021-10-01 18:48:00 +0000

    uncomment rich-text

--- src/components.js
+++ src/components.js
@@ -41,7 +41,7 @@ lazyLoad('search', '[search_id]', ()=> import (/*webpackChunkName: "search-chunk
 
 // Collabortion Components
 lazyLoad('text', 'input, textarea, [contenteditable]', ()=> import (/*webpackChunkName: "text-chunk"*/ '@cocreate/text'));
-// lazyLoad('rich-text', '[actions*="nodeName"], [actions*="cloneElement"], [actions*="deleteElement"]', ()=> import (/*webpackChunkName: "rich-text-chunk"*/ '@cocreate/rich-text'));
+lazyLoad('rich-text', '[actions*="nodeName"], [actions*="cloneElement"], [actions*="deleteElement"]', ()=> import (/*webpackChunkName: "rich-text-chunk"*/ '@cocreate/rich-text'));
 
 // UI Components
 lazyLoad('floatingLabel', '.floating-label', ()=> import (/*webpackChunkName: "floating-label-chunk"*/ '@cocreate/floating-label'));
