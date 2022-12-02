b42f4f90b4fcd961f391d49e9486f9b9df8e3f3d
Frank Pagan <frank@cocreate.app>
2022-12-02 14:46:39 +0000

    fix: createUser renamed to signUp, filter-sort-type renamed to filter-sort-direction

--- src/components.js
+++ src/components.js
@@ -40,7 +40,7 @@ lazyLoad('scroll', '[scroll]', ()=> import (/*webpackChunkName: "scroll-chunk"*/
 lazyLoad('resize', '[resize]', ()=>import(/*webpackChunkName: "resize-chunk"*/ '@cocreate/resize'));
 lazyLoad('remove', '[actions*="remove"]', ()=>import(/*webpackChunkName: "remove-chunk"*/ '@cocreate/remove'));
 lazyLoad('attributes', '[attribute]', ()=>import(/*webpackChunkName: "attributes-chunk"*/ '@cocreate/attributes'));
-lazyLoad('users', '[actions*="createUser"], [actions*="login"], [actions*="logout"], [user-status]', ()=> import (/*webpackChunkName: "users-chunk"*/ '@cocreate/users'));
+lazyLoad('users', '[actions*="signUp"], [actions*="signIn"], [actions*="signOut"], [user-status]', ()=> import (/*webpackChunkName: "users-chunk"*/ '@cocreate/users'));
 lazyLoad('unique', '[unique]', ()=> import (/*webpackChunkName: "unique-chunk"*/ '@cocreate/unique'));
 lazyLoad('organizations', '[actions*="createOrg"], [actions*="deleteOrg"]', ()=> import (/*webpackChunkName: "organizations-chunk"*/ '@cocreate/organizations'));
 lazyLoad('industry', '[actions*="createIndustry"], [actions*="runIndustry"], [actions*="deleteIndustry"], [actions*="deleteIndustries"]', ()=> import (/*webpackChunkName: "industry-chunk"*/ '@cocreate/industry'));
