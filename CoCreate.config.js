1d9c71f3013553c403ad2eeffa865608c30018be
Frank Pagan <frank@cocreate.app>
2022-12-30 22:03:35 +0000

    fix: update config to use new directories, bump cdn to 1.37.0

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -4,175 +4,34 @@ module.exports = {
         "organization_id": "5ff747727005da1c272740ab",
         "host": "general.cocreate.app"
     },
+    "directories": [
+        {
+            "entry": "./docs",
+            "exclude": [
+                "demo"
+            ],
+            "collection": "files",
+            "document": {
+                "name": "{{name}}",
+                "src": "{{source}}",
+                "domains": [
+                    "*",
+                    "general.cocreate.app"
+                ],
+                "directory": "/docs/{{directory}}",
+                "path": "{{path}}",
+                "content-type": "{{content-type}}",
+                "public": "true",
+                "website_id": "61381ed8829b690010a4f2b2"
+            }
+        }
+    ],
     "sources": [
         {
-            "entry": "./docs/apikey.js",
-            "collection": "files",
-            "document": {
-                "_id": "619c47f2a8b6b4001a9e1fae",
-                "name": "apikey.js",
-                "src": "{{source}}",
-                "domains": [
-                    "*",
-                    "general.cocreate.app"
-                ],
-                "path": "/docs/apikey.js",
-                "directory": "/docs",
-                "content-type": "application/javascript",
-                "public": "true",
-                "website_id": "5ffbceb7f11d2d00103c4535"
-            }
-        },
-        {
-            "entry": "./docs/index.html",
-            "collection": "files",
-            "document": {
-                "_id": "60907b078bc9a6631d1d0ac4",
-                "name": "index.html",
-                "src": "{{source}}",
-                "domains": [
-                    "*",
-                    "general.cocreate.app"
-                ],
-                "path": "/docs/index.html",
-                "directory": "/docs",
-                "content-type": "text/html",
-                "public": "true",
-                "website_id": "5ffbceb7f11d2d00103c4535"
-            }
-        },
-        {
-            "entry": "./docs/elements.html",
-            "collection": "files",
-            "document": {
-                "_id": "60907f898bc9a6631d1d0ad8",
-                "name": "elements.html",
-                "src": "{{source}}",
-                "domains": [
-                    "*",
-                    "general.cocreate.app"
-                ],
-                "path": "/docs/elements.html",
-                "directory": "/docs",
-                "content-type": "text/html",
-                "public": "true",
-                "website_id": "5ffbceb7f11d2d00103c4535"
-            }
-        },
-        {
-            "entry": "./docs/logic.html",
-            "collection": "files",
-            "document": {
-                "_id": "61a2ff48a8b6b4001a9fce9e",
-                "name": "logic.html",
-                "src": "{{source}}",
-                "domains": [
-                    "*",
-                    "general.cocreate.app"
-                ],
-                "path": "/docs/logic.html",
-                "directory": "/docs",
-                "content-type": "text/html",
-                "public": "true",
-                "website_id": "5ffbceb7f11d2d00103c4535"
-            }
-        },
-        {
-            "entry": "./docs/user-management.html",
-            "collection": "files",
-            "document": {
-                "_id": "61bf9089a8b6b4001aaba266",
-                "name": "user-management.html",
-                "src": "{{source}}",
-                "domains": [
-                    "*",
-                    "general.cocreate.app"
-                ],
-                "path": "/docs/user-management.html",
-                "directory": "/docs",
-                "content-type": "text/html",
-                "public": "true",
-                "website_id": "5ffbceb7f11d2d00103c4535"
-            }
-        },
-        {
-            "entry": "./docs/objects.html",
-            "collection": "files",
-            "document": {
-                "_id": "60888216117c640e75963040",
-                "name": "objects.html",
-                "src": "{{source}}",
-                "domains": [
-                    "*",
-                    "general.cocreate.app"
-                ],
-                "path": "/docs/objects.html",
-                "directory": "/docs",
-                "content-type": "text/html",
-                "public": "true",
-                "website_id": "5ffbceb7f11d2d00103c4535"
-            }
-        },
-        {
-            "entry": "./docs/templating.html",
-            "collection": "files",
-            "document": {
-                "_id": "61a29714a8b6b4001a9fb48a",
-                "name": "templating.html",
-                "src": "{{source}}",
-                "domains": [
-                    "*",
-                    "general.cocreate.app"
-                ],
-                "path": "/docs/templating.html",
-                "directory": "/docs",
-                "content-type": "text/html",
-                "public": "true",
-                "website_id": "5ffbceb7f11d2d00103c4535"
-            }
-        },
-        {
-            "entry": "./docs/components/menu.html",
-            "collection": "files",
-            "document": {
-                "_id": "603717b07de7fb350ae9fec8",
-                "name": "menu.html",
-                "src": "{{source}}",
-                "domains": [
-                    "*",
-                    "general.cocreate.app"
-                ],
-                "path": "/docs/components/menu.html",
-                "directory": "/docs/components",
-                "content-type": "text/html",
-                "public": "true",
-                "website_id": "5ffbceb7f11d2d00103c4535"
-            }
-        },
-        {
-            "entry": "./docs/components/navbar.html",
-            "collection": "files",
-            "document": {
-                "_id": "60395ef42b3ac232657040fd",
-                "name": "navbar.html",
-                "src": "{{source}}",
-                "domains": [
-                    "*",
-                    "general.cocreate.app"
-                ],
-                "path": "/docs/components/navbar.html",
-                "directory": "/docs/components",
-                "content-type": "text/html",
-                "public": "true",
-                "website_id": "5ffbceb7f11d2d00103c4535"
-            }
-        },
-        {
-            "entry": "./demo/elements/input.html",
             "collection": "demos",
             "document": {
                 "_id": "619aa8e8a8b6b4001a9dc24e",
-                "input-demo": "{{source}}"
+                "input-demo": "{{./demo/elements/input.html}}"
             }
         },
         {
@@ -180,7 +39,7 @@ module.exports = {
             "collection": "demos",
             "document": {
                 "_id": "619bd887a8b6b4001a9e05af",
-                "textarea-demo": "{{source}}"
+                "textarea-demo": "{{./demo/elements/textarea.html}}"
             }
         }
     ]
