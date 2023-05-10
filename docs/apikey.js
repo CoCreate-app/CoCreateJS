6ee723392de93b0b59721f107304c43c0fb2e920
frankpagan <frank@cocreate.app>
2023-05-10 14:32:20 -0600

    fix: apikey renamed to key

--- docs/apikey.js
+++ docs/apikey.js
@@ -1,6 +1,6 @@
              
     var CoCreateConfig = {                 
-        apiKey: '2061acef-0451-4545-f754-60cf8160',                 
+        key: '2061acef-0451-4545-f754-60cf8160',                 
         organization_id: '5ff747727005da1c272740ab',                 
         host: 'general.cocreate.app'
     } 
