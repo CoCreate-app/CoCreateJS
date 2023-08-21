e791450d76821355a447cd572a60fecba154eecd
frankpagan <frank@cocreate.app>
2023-08-20 18:12:32 -0600

    add dist to directories

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -4,6 +4,25 @@ module.exports = {
     "host": "",
     "directories": [
         {
+            "entry": "./dist",
+            "exclude": [
+                ".txt"
+            ],
+            "array": "files",
+            "object": {
+                "name": "{{name}}",
+                "src": "{{source}}",
+                "host": [
+                    "*",
+                    "general.cocreate.app"
+                ],
+                "directory": "/dist/{{directory}}",
+                "path": "{{path}}",
+                "content-type": "{{content-type}}",
+                "public": "true"
+            }
+        },
+        {
             "entry": "./docs",
             "exclude": [
                 "demo"
