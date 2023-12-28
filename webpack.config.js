ef53407c9e9402cceade713798b1271b80b0a45f
frankpagan <frank@cocreate.app>
2023-12-27 20:54:56 -0500

    feat: plugins loaded from @cocreate/webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-const fs = require('fs');
+// const fs = require('fs');
 const path = require('path');
-const upload = require('@cocreate/cli/src/commands/upload.js')
+const CoCreateConfig = require('./CoCreate.config')
+const { ModuleGenerator, fileUploader, SymlinkCreator } = require('@cocreate/webpack')
 
 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
@@ -32,49 +33,9 @@ module.exports = async (env, argv) => {
         },
 
         plugins: [
-            {
-                apply: (compiler) => {
-                    const CoCreateConfig = require('./CoCreate.config.js') || {};
-                    if (!CoCreateConfig.modules) {
-                        CoCreateConfig.modules = {}
-                    }
-
-                    let modulesGenerated = false
-
-                    compiler.hooks.beforeCompile.tapAsync('CoCreateLazyloader', (params, callback) => {
-                        if (modulesGenerated)
-                            callback();
-                        else {
-                            let outputPath = CoCreateConfig.modules.outputPath || './modules.js'; // Default path for generated module.js
-
-                            // Generate module content based on CoCreateConfig
-                            let moduleContent = `import { dependency, lazyLoad } from '@cocreate/lazy-loader';\n\n`;
-                            Object.entries(CoCreateConfig.modules).forEach(([moduleName, moduleInfo]) => {
-                                if (moduleName === 'outputPath' || typeof moduleInfo !== 'object') return;
-                                if (moduleInfo.selector) {
-                                    // Generate lazyLoad statements for modules with selectors
-                                    moduleContent += `lazyLoad('${moduleName}', '${moduleInfo.selector}', () => import(/*webpackChunkName: "${moduleName}-chunk"*/ '${moduleInfo.import}'));\n`;
-                                } else {
-                                    // Generate dependency statements for other modules
-                                    moduleContent += `dependency('${moduleName}', import(/*webpackChunkName: "${moduleName}-chunk"*/ '${moduleInfo.import}'));\n`;
-                                }
-                            });
-
-                            // Write the module content to the specified outputPath
-                            fs.writeFile(outputPath, moduleContent, (err) => {
-                                if (err) {
-                                    console.error(`Error writing ${outputPath}:`, err);
-                                    callback(err); // Handle errors in async hook
-                                } else {
-                                    modulesGenerated = true
-                                    console.log(`${outputPath} generated successfully.`);
-                                    callback(); // Proceed with compilation
-                                }
-                            });
-                        }
-                    });
-                },
-            },
+            new ModuleGenerator(CoCreateConfig.modules),
+            new fileUploader(env),
+            new SymlinkCreator(),
             new CleanWebpackPlugin(),
             new MiniCssExtractPlugin({
                 filename: isProduction ? '[name].min.css' : '[name].css',
@@ -168,56 +129,6 @@ module.exports = async (env, argv) => {
 
     };
 
-    // Hook into the 'done' event of the compiler to execute code when the build is complete.
-    if (!isProduction) {
-        config.plugins.push({
-            apply: (compiler) => {
-                symlink('./dist', '../dist', 'dir')
-                symlink('./node_modules/@cocreate/pwa/src/service-worker.js', '../service-worker.js', 'file')
-                symlink('./node_modules/@cocreate/pwa/src/manifest.webmanifest', '../manifest.webmanifest', 'file')
-                symlink('./node_modules/@cocreate/pwa/src/offline.html', '../offline.html', 'file')
-            },
-        });
-    }
-
-    if (isWatch) {
-        if (env.beforeCompilation)
-            upload(__dirname, ['../', '-w'])
-        else if (env.afterCompilation) {
-            config.plugins.push({
-                apply: (compiler) => {
-                    // Hook into the "emit" event, which occurs after the compilation is complete
-                    compiler.hooks.emit.tapAsync('watchFiles', (compilation, callback) => {
-                        if (!isWatching) {
-                            isWatching = true
-                            upload(__dirname, ['../', '-w'])
-                        }
-                        // Don't forget to call the callback function to signal completion
-                        callback();
-                    });
-                }
-            })
-        }
-    }
-
     return config;
 
-}
-
-function symlink(target, destination, option) {
-    if (fs.existsSync(target)) {
-        target = path.resolve(target)
-
-        if (!fs.existsSync(destination)) {
-            destination = path.resolve(destination)
-
-            fs.symlink(target, destination, option, (err) => {
-                if (err)
-                    console.log(err);
-                else
-                    console.log("symlink added: ", target);
-            })
-
-        }
-    }
 }
